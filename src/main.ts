82855c9af970e112fb2e75cec59ee23299ba352e
Irina Zadorina <160423489+i-zadorina@users.noreply.github.com>
2024-07-25 18:31:19 +0300

    added links & notice

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as d3 from 'd3'
 import * as topojson from 'topojson-client'
-import { countries } from './countries'
+import { countries, TaxSummary } from './countries'
 
 // Set up the SVG canvas dimensions
 const width = 960
@@ -29,7 +29,7 @@ interface Geometry {
 
 interface Properties {
 	name: string
-	value: number
+	taxSummary?: TaxSummary
 }
 
 const path = d3.geoPath().projection(projection)
@@ -37,7 +37,7 @@ const path = d3.geoPath().projection(projection)
 // Create a color scale
 const colorScale = d3.scaleSequential(d3.interpolateRgb('#96d363', '#b80707')).domain([0, 0.8])
 function fill(d: Country) {
-	const value = d.properties.value
+	const value = d.properties.taxSummary?.percentage
 	return value === undefined ? '#d3d1d1' : colorScale(value)
 }
 
@@ -60,15 +60,14 @@ loadExchangeRates()
 				const geoData = topojson.feature(topoData, topoData.objects.countries) as unknown as {
 					features: Country[]
 				}
-				geoData.features.forEach((feature: any) => {
+				geoData.features.forEach((feature) => {
 					const countryName = feature.properties.name
 					const taxStrategy = countries[countryName]
 					if (taxStrategy) {
-						const taxResult = taxStrategy({ incomeUSD: 100000 }, exchangeRates.rates)
-						feature.properties.value = taxResult.percentage
+						const taxResult = taxStrategy({ incomeUSD: 100000, married: false, oneIncome: true }, exchangeRates.rates)
+						feature.properties.taxSummary = taxResult
 					} else {
 						console.error(`No tax strategy found for country ${countryName}`)
-						feature.properties.value = 0
 					}
 				})
 
@@ -82,15 +81,18 @@ loadExchangeRates()
 					// @ts-ignore
 					.attr('d', path)
 					.attr('fill', fill)
-					.on('mouseover', function (event: MouseEvent, d: Country) {
+					.on('mouseover', function (event: MouseEvent, feature: Country) {
 						d3.select(this).attr('fill', 'orange')
 						tooltip.transition().duration(200).style('opacity', 0.9)
 						tooltip
-							.html(`${d.properties.name}<br>Tax rate: ${formatTaxRate(d.properties.value)}`)
+							.html(`${feature.properties.name}<br>
+								Tax rate: ${formatTaxRate(feature.properties.taxSummary?.percentage)}<br>
+								${createLink(feature.properties.taxSummary?.link)}`)
 							.style('left', event.pageX + 'px')
 							.style('top', event.pageY - 28 + 'px')
 					})
-					.on('mouseout', function (_: MouseEvent, d: Country) {
+					.on('mouseout', function (e: MouseEvent, d: Country) {
+						// alert(e.relatedTarget)
 						d3.select(this).attr('fill', fill(d))
 						tooltip.transition().duration(500).style('opacity', 0)
 					})
@@ -108,4 +110,9 @@ loadExchangeRates()
 
 function formatTaxRate(rate: number | undefined) {
 	return rate === undefined ? 'n/a' : (rate * 100).toFixed(2) + '%'
+}
+function createLink(link: string | undefined){
+  if (link){
+    return `<a href="${link}">More details...</a>`
+  }
 }
